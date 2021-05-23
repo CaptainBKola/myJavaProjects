21ca7cce15b98168c073a72a4ea004b18580e1d5
Badmus Kola <badmuskola1989@gmail.com>
2021-05-23 02:49:30 +0100

    created a makeshift to handle main branch content

--- src/chapterThreeExercises/Student.java
+++ src/chapterThreeExercises/Student.java
@@ -1,28 +1,62 @@
 package chapterThreeExercises;
 
-public class Student {
+public class Student
+{
+        private String name;
+        private double average;
+        // constructor initializes instance variables
+        public Student(String constructorName, double constructorAverage)
+        {
+            name = constructorName;
+            // validate that average is > 0.0 and <= 100.0; otherwise,
+             // keep instance variable average's default value (0.0)
 
-   private int studentScore;
+            if (constructorAverage <= 100.0)
+                if (constructorAverage > 0.0)
+                     average = constructorAverage; // assign to instance variable
+             }
 
-    void  setAverageScore(int studentScore){
-        this.studentScore = studentScore;
+        // sets the chapterThreeExercises.Student's name
+         public void setName(String studentName)
+         {
+             name = studentName;}
 
-    }
+         // retrieves the chapterThreeExercises.Student's name
+         public String getName() {
+             return name;
+             }
 
-    public String getScoringGrade() {
+         // sets the chapterThreeExercises.Student's average
+         public void setAverage(double studentAverage) {
+             // validate that average is > 0.0 and <= 100.0; otherwise,
+             // keep instance variable average's current value
+             if (studentAverage > 0.0)
+                 if (studentAverage <= 100.0)
+                     average = studentAverage; // assign to instance variable 40 }
+         }
+             // retrieves the chapterThreeExercises.Student's average
 
-        String scoringGrade = ""; // initialized to empty String
-        if (studentScore >= 90.0)
-            scoringGrade = "A";
-        else if (studentScore >= 80.0)
-            scoringGrade = "B";
-        else if (studentScore >= 70.0)
-            scoringGrade = "C";
-        else if (studentScore >= 60.0)
-            scoringGrade = "D";
-        else
-            scoringGrade = "F";
-        return scoringGrade;
-    }
+        public double getAverage(){
+                 return average;
+             }
 
-}
+             // determines and returns the chapterThreeExercises.Student's letter grade
+        public String getScoringGrade(){
+
+            String scoringGrade = ""; // initialized to empty String
+                 if (average >= 90.0)
+                     scoringGrade= "A";
+                 else if (average >= 80.0)
+                     scoringGrade= "B";
+                 else if (average >= 70.0)
+                     scoringGrade = "C";
+                 else if (average >= 60.0)
+                     scoringGrade = "D";
+                 else
+                     scoringGrade = "F";
+                return scoringGrade;
+             }
+
+
+
+} // end class chapterThreeExercises.Student
--- test/chapterThreeExercises/StudentTest.java
+++ test/chapterThreeExercises/StudentTest.java
@@ -1,17 +1,31 @@
-package chapterThreeExercises;
-
+import chapterThreeExercises.Student;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class StudentTest {
 
     @Test
-    public void getGradesOfStudents(){
+    public void  setAverageGrade(){
+        Student studentGrade = new Student("Kola",20);
+        studentGrade.setAverage(30);
+        assertEquals(30,studentGrade.getAverage());
+        System.out.print(studentGrade.getAverage());
 
-        Student  studentGrade = new Student();
-        studentGrade.setAverageScore(54);
+    }
+    @Test
+    public void setStudentName(){
+        Student studentGrade = new Student("Kunle",10);
+        studentGrade.setName("Bayo");
+        assertEquals("Bayo",studentGrade.getName());
+
+    }
+
+    @Test
+    public void getGradesOfStudents(){
+        Student  studentGrade = new Student("Kunle",50);
         assertEquals("F",studentGrade.getScoringGrade());
+
     }
 
 
